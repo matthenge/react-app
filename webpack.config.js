04d746e39fe0e24b3b5d370df9d76754d7ac655c
James Maruhi <jamesmaruhi@Jamess-MacBook-Pro.local>
2019-04-21 19:29:40 +0300

    final deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
+const path=require('path');
 module.exports = {
     entry: [
         './src/index.js'
     ],
     output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: './dist/main.js'
+        path:path.resolve(__dirname,'dist'),
+        filename: './dist/main.js',
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -20,16 +20,43 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(gif|svg|jpg|png)$/,
-                loader: 'file-loader',
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
             }
         ]
     },
     devServer: {
         historyApiFallback: true,
+        inline:true
     },
     plugins: [
         new HtmlWebPackPlugin({
